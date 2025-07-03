b347ff81dc440cb9a48ae2825f25a1c99c7d4bda
wahyualif23014 <zexvulca@gmail.com>
2025-07-03 15:33:04 +0700

    new saja hari ini

--- lib/features/activities/data/repositories/activity_repository.dart
+++ lib/features/activities/data/repositories/activity_repository.dart
@@ -1,21 +1,19 @@
 // lib/features/activities/domain/repositories/activity_repository.dart
 
-import 'package:dartz/dartz.dart'; // Menggunakan Left (Failure/Exception) dan Right (Success)
-import '../../domain/entity/activity_entity.dart'; // Import ActivityEntity
+import 'package:dartz/dartz.dart';
+import '../../domain/entity/activity_entity.dart';
 
 abstract class ActivityRepository {
-  // Mengambil daftar semua aktivitas
+  // daftar semua aktivitas
   Future<Either<Exception, List<ActivityEntity>>> getActivities();
 
-  // Mengambil aktivitas berdasarkan ID
+  // aktivitas berdasarkan ID
   Future<Either<Exception, ActivityEntity>> getActivityById(String id);
 
   // Menambahkan aktivitas baru
-  // Menerima ActivityEntity, akan diubah menjadi ActivityModel di impl
   Future<Either<Exception, ActivityEntity>> addActivity(ActivityEntity activity);
 
   // Mengupdate aktivitas yang sudah ada
-  // Menerima ActivityEntity, akan diubah menjadi ActivityModel di impl
   Future<Either<Exception, ActivityEntity>> updateActivity(ActivityEntity activity);
 
   // Menghapus aktivitas berdasarkan ID
